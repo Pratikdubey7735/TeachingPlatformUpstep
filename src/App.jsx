b566e547636fb6642a7a9d8f5653f4e9dc2b1745
Pratik Dubey <146483524+Pratikdubey7735@users.noreply.github.com>
2024-11-13 08:33:02 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 // src/App.jsx
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Home from "./Pages/Home.jsx";
-import Dashboard from "./Pages/Dashboard.jsx";
-import Login from "./components/Login.jsx";
-import Setup from "./components/setup.jsx";
-import Navbar from "./components/Navbar.jsx";
+import Home from "./Pages/Home";
+import Dashboard from "./pages/Dashboard";
+import Login from "./components/Login";
+import Setup from "./components/setup";
+import Demo from "./components/Demo";
+import Play from "./components/Play";
+import Navbar from "./components/Navbar";
 import { AuthProvider } from "./context/AuthContext";
-import ProtectedRoute from "./components/ProtectedRoute.jsx";
+import ProtectedRoute from "./components/ProtectedRoute";
 function App() {
   return (
     <AuthProvider>
@@ -18,6 +20,8 @@ function App() {
         {/* Use ProtectedRoute for protected pages */}
         <Route path="/dashboard" element={<ProtectedRoute element={Dashboard} />} />
         <Route path="/setup" element={<ProtectedRoute element={Setup} />} />
+        <Route path="/demo" element={<ProtectedRoute element={Demo} />} />
+        <Route path="/play" element={<ProtectedRoute element={Play} />} />
       </Routes>
     </AuthProvider>
   );

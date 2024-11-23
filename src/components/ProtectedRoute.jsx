aac08c7695624d404a1754dc41da279e0ac17464
Pratik Dubey <146483524+Pratikdubey7735@users.noreply.github.com>
2024-11-23 20:01:05 +0530

    Update ProtectedRoute.jsx

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,16 +1,14 @@
-// src/components/ProtectedRoute.jsx
 import React from "react";
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
-
 const ProtectedRoute = ({ element: Component }) => {
-  const { isLoggedIn } = useAuth(); // Updated here
-
-  // If the user is not authenticated, redirect to login
-  if (!isLoggedIn) { // Updated here
+  const { isLoggedIn } = useAuth();
+  // Redirect to login if not authenticated
+  if (!isLoggedIn) {
     return <Navigate to="/login" replace />;
   }
-  // Otherwise, render the component
+
+  // Render the component if authenticated
   return <Component />;
 };
 

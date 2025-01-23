48d6b36a79c00942a84799d05876eeb8e4182060
Pratik Dubey <146483524+Pratikdubey7735@users.noreply.github.com>
2025-01-23 15:57:40 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'xs': '480px',   // Custom breakpoint for extra small devices
+        'sm': '640px',   // Small screens and up
+        'md': '768px',   // Medium screens and up
+        'lg': '1024px',  // Large screens and up
+        'xl': '1280px',  // Extra large screens and up
+        '2xl': '1536px', // 2x Extra large screens and up
+        'max-sm': { max: '639px' }, // Custom max-width breakpoint for small screens
+      },
+    },
   },
   plugins: [],
-}
+};
